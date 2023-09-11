7960bc423b8febcd4aa90afb16cd806a4b89b734
Nagbhushan Tirth <113358781+nt56@users.noreply.github.com>
2023-09-11 08:12:23 +0530

    Update Day22.java

--- Day22.java
+++ Day22.java
@@ -20,3 +20,23 @@ public class PerfectSquare {
 
 }
 //perfect square --> Is a number that can be expressed as the product of an integer by itself or as the second exponent of an integer
+
+
+import java.util.Scanner;
+
+public class PerfectSquare {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Number : ");
+        int num = sc.nextInt();
+        for(int i=1; i<num/2; i++){
+            if((i * i) == num){
+                System.out.println("Given number is a Square of " +i);
+                break;
+            }
+        }
+    }
+
+}
+//method-2
